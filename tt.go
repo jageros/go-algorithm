f6f92971c9e5c04619945301a9444e849f610c02
Jay Leung <lhj168os@gmail.com>
2019-07-19 12:59:32 +0800

    tt modify

--- tt.go
+++ tt.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
+
+func main() {
+	t := "2019-07-04 19:45:00"
+	loc, _ := time.LoadLocation("Local")
+	tim, err := time.ParseiInLocation("2006-01-02 15:04:05", t, loc)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	}
+
+	args := []string{"Hello", "world", "!"}
+	fn := func() {
+		fmt.Printf("len: %d\t%v\n", len(args), args)
+	}
+
+	task(tim, fn)
+
+	fmt.Println("Run end!")
+}
+
+func task(tim time.Time, fn func()) {
+	if tim.Before(time.Now()) {
+		return
+	}
+	dd := tim.Sub(time.Now())
+	timer := time.NewTimer(dd)
+	<-timer.C
+	fn()
+}
 
 //func tt(w *sync.WaitGroup, l *sync.Mutex, i int) {
 //	defer func() {
@@ -29,37 +56,35 @@ import (
 //	fmt.Println("Run end!")
 //}
 
-
-func tt(cd *sync.Cond, wg *sync.WaitGroup, i int) {
-	defer wg.Done()
-	cd.L.Lock()
-	fmt.Printf("lock: %d\n", i)
-	cd.Wait()
-	fmt.Printf("wait end: %d\n", i)
-	cd.L.Unlock()
-}
-
-func main() {
-	l := &sync.Mutex{}
-	w := &sync.WaitGroup{}
-	cd := sync.NewCond(l)
-
-	for i := 0; i < 10; i++ {
-		w.Add(1)
-		go tt(cd, w, i)
-	}
-	time.Sleep(time.Second)
-	fmt.Println("Send 5 Signal:")
-	for i := 0; i < 5; i++ {
-		cd.Signal()   //唤醒一个wait中的goroutine
-	}
-	time.Sleep(time.Second)
-	fmt.Println("Send Broadcast:")
-	cd.Broadcast()  //广播唤醒所有wait中的goroutine
-	w.Wait()
-	fmt.Println("Run end!")
-}
-
+//func tt(cd *sync.Cond, wg *sync.WaitGroup, i int) {
+//	defer wg.Done()
+//	cd.L.Lock()
+//	fmt.Printf("lock: %d\n", i)
+//	cd.Wait()
+//	fmt.Printf("wait end: %d\n", i)
+//	cd.L.Unlock()
+//}
+//
+//func main() {
+//	l := &sync.Mutex{}
+//	w := &sync.WaitGroup{}
+//	cd := sync.NewCond(l)
+//
+//	for i := 0; i < 10; i++ {
+//		w.Add(1)
+//		go tt(cd, w, i)
+//	}
+//	time.Sleep(time.Second)
+//	fmt.Println("Send 5 Signal:")
+//	for i := 0; i < 5; i++ {
+//		cd.Signal()   //唤醒一个wait中的goroutine
+//	}
+//	time.Sleep(time.Second)
+//	fmt.Println("Send Broadcast:")
+//	cd.Broadcast()  //广播唤醒所有wait中的goroutine
+//	w.Wait()
+//	fmt.Println("Run end!")
+//}
 
 //func tt(wg *sync.WaitGroup, i int) {
 //	defer wg.Done()
